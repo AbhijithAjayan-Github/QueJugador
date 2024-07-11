10f6b19d29f97802baca05b9cd53fe3817944a9f
AbhijithAjayan-Github <abhijithajayan21262@gmail.com>
2024-07-11 16:11:36 +0530

    modified entity attributes

--- Models/Entity/Player.cs
+++ Models/Entity/Player.cs
@@ -5,8 +5,15 @@ using System.Threading.Tasks;
 
 namespace QueJugadorApp.Models.Entity
 {
-    public class Players
+    public class Player
     {
-        
+        // Unique Id
+      public Guid Id {get;set;}   
+      public string PlayerName {get;set;}
+      public string PlayerAddress{get;set;}
+      public string PlayerMail {get;set;}
+      public string playerPhone {get;set;}
+      public string PlayerPosition {get;set;}
+      Public string PlayerClub {get;set;}  
     }
 }
